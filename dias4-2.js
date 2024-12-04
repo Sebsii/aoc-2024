78d8a12d6643235854e0cba9e77dd387330fb36b
Sebsii <s.seebacher94@gmx.at>
2024-12-04 18:07:04 +0100

    ayayaya numero 4

---
+++ dias4-2.js
@@ -0,0 +1,33 @@
+﻿import {lines} from './input_provider.js'
+
+const linesss = await lines(4);
+
+let sum = 0;
+
+for (let i = 1; i < linesss.length - 1; i++) {
+    for (let j = 1; j < linesss[i].length - 1; j++) {
+        let x = linesss[i][j];
+        if (x !== 'A') continue;
+        sum += check(i, j);
+    }
+}
+
+console.log(sum);
+
+function check(i, j) {
+    const word = 'MAS'
+    const drow = 'SAM'
+    const a = 'A'
+    let tr = linesss[i - 1][j + 1];
+    let tl = linesss[i - 1][j - 1];
+    let br = linesss[i + 1][j + 1];
+    let bl = linesss[i + 1][j - 1];
+
+    if (tl + a + br === word || tl + a + br === drow) {
+        if (tr + a + bl === word || tr + a + bl === drow) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
---
+++ dias4.js
@@ -0,0 +1,35 @@
+﻿import {lines} from './input_provider.js'
+
+const linesss = await lines(4);
+
+let sum = 0;
+
+for (let i = 0; i < linesss.length; i++) {
+    for (let j = 0; j < linesss[i].length; j++) {
+        let x = linesss[i][j];
+        if (x !== 'X') continue;
+        sum += check(i, j);
+    }
+}
+
+console.log(sum);
+
+function check(i, j) {
+    const word = 'XMAS'
+    let count = 0;
+    for (let k = -1; k <= 1; k++) {
+        for (let l = -1; l <= 1; l++) {
+            if (k === 0 && l === 0) continue;
+            let current = 'X';
+            for (let m = 1; m <= 3; m++) {
+                if (linesss[i + k * m] === undefined) continue;
+                if (linesss[i + k * m][j + l * m] === undefined) continue;
+                current += linesss[i + k * m][j + l * m]
+            }
+            if (current === word)
+                count++;
+        }
+    }
+
+    return count;
+}
