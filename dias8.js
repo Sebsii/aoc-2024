f7cb37ad7c1b21d43f4bc83128d01b59eaf68d5b
Sebsii <s.seebacher94@gmx.at>
2024-12-08 12:23:10 +0100

    we are a thrid of the way there

---
+++ dias8.js
@@ -0,0 +1,52 @@
+﻿import {twoD} from './input_provider.js'
+
+const map = await twoD(8);
+const antiMap = structuredClone(map);
+const antennas = {};
+
+for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+        const antenna = map[i][j];
+        if (antenna === '.') continue;
+        antennas[antenna] ??= [];
+        antennas[antenna].push([i, j]);
+    }
+}
+
+Object.keys(antennas).forEach(a => {
+    const arr = antennas[a];
+    for (let i = 0; i < arr.length - 1; i++) {
+        let [dix, diy] = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            let [djx, djy] = arr[j];
+            let [dx, dy] = [djx - dix, djy - diy];
+            try {
+                let x = 0;
+                while(true) {
+                    antiMap[dix - dx * x][diy - dy * x] = '#';
+                    x++;
+                }
+            } catch (e) {
+            }
+            try {
+                let x = 0;
+                while(true) {
+                    antiMap[djx + dx * x][djy + dy * x] = '#';
+                    x++;
+                }
+            } catch (e) {
+            }
+        }
+    }
+})
+
+let sum = 0;
+for (let i = 0; i < map.length; i++){
+    for (let j = 0; j < map[0].length; j++){
+        if (antiMap[i][j] === '#') {
+            sum++;
+        }
+    }
+}
+
+console.log(sum);
