40e2e0d13825ad01c452d6c36f0721684a875e50
Sebsii <s.seebacher94@gmx.at>
2024-12-09 23:32:26 +0100

    holy moly, day 9 by far the hardest so far

---
+++ dias9-2.js
@@ -0,0 +1,53 @@
+﻿import {lines} from './input_provider.js'
+
+const line = await lines(9);
+const l = line[0];
+
+let blockCount = 0;
+for (let i = 0; i < l.length; i = i + 2) {
+    blockCount += parseInt(l[i]);
+}
+
+let sum = 0
+let arrrrrrr = []
+let ranges = []
+for (let i = 0; i < l.length; i++) {
+    let count = parseInt(l[i]);
+    let entry = [];
+    entry.push(arrrrrrr.length)
+    for (let j = 0; j < count; j++) {
+        let id = i / 2;
+        if (i % 2 === 0) {
+            arrrrrrr.push(id)
+        } else {
+            arrrrrrr.push('X');
+        }
+    }
+    entry.push(count)
+    ranges.push(entry)
+}
+
+for (let b = ranges.length - 1; b >= 0; b -= 2) {
+    let [startB, sizeB] = ranges[b];
+    let id = b / 2;
+    for (let f = 1; f < ranges.length; f += 2) {
+        let [startF, sizeF] = ranges[f];
+        if (startF >= startB) break;
+        if (sizeF >= sizeB) {
+            for (let x = 0; x < sizeB; x++) {
+                arrrrrrr[x + startF] = id;
+                arrrrrrr[x + startB] = 'X';
+            }
+            ranges[f] = [startF + sizeB, sizeF - sizeB]
+            break;
+        }
+    }
+}
+
+for (let i = 0; i < arrrrrrr.length; i++) {
+    let el = arrrrrrr[i];
+    if (el === 'X') continue;
+    sum += i * el;
+}
+
+console.log(sum);
---
+++ dias9.js
@@ -0,0 +1,88 @@
+﻿import {lines} from './input_provider.js'
+
+const line = await lines(9);
+const l = line[0];
+
+let blockCount = 0;
+for (let i = 0; i < l.length; i = i + 2) {
+    blockCount += parseInt(l[i]);
+}
+
+console.log('block count: ' + blockCount);
+
+// *** Tried some stuff here but got too complicated (-‿-") ***
+
+// let sum = 0;
+//
+// let blockPosCounter = 0;
+// let diskPointer = 0;
+// let fileIdBackward = (l.length + 1) / 2;
+// let diskPointerBackward = l.length - 1;
+// let fileIdForward = 0;
+// let backFileIdx = 0;
+// let backFileCount = 0;
+//
+// while (true) {
+//     let isFile = diskPointer % 2 === 0;
+//     let count = l[diskPointer];
+//
+//     if (isFile) {
+//         for (let i = 0; i < count; i++) {
+//             sum += blockPosCounter * fileIdForward
+//             blockPosCounter++;
+//         }
+//         fileIdForward++;
+//     } else {
+//         for (let i = 0; i < count; i++) {
+//             backFileCount = l[diskPointerBackward];
+//             let id;
+//             if (backFileIdx < backFileCount) {
+//                 id = fileIdBackward;
+//                 backFileIdx++;
+//             } else {
+//                 diskPointerBackward -= 2;
+//                 fileIdBackward--;
+//                 backFileIdx = 1;
+//                 id = fileIdBackward;
+//             }
+//             id = fileIdBackward
+//             sum += blockPosCounter * id
+//             blockPosCounter++;
+//         }
+//     }
+//
+//     diskPointer++;
+//     if (fileIdForward >= fileIdBackward) break;
+// }
+
+let sum = 0
+let arrrrrrr = []
+for (let i = 0; i < l.length; i++) {
+    let count = l[i];
+    for (let j = 0; j < count; j++) {
+        let id = i / 2;
+        if (i % 2 === 0) {
+            arrrrrrr.push(id)
+        } else {
+            arrrrrrr.push('X');
+        }
+    }
+}
+
+let b = arrrrrrr.length - 1;
+
+for (let i = 0; i < arrrrrrr.length; i++) {
+    if (i >= b) break;
+    let id = arrrrrrr[i];
+    if (id === 'X') {
+        while (arrrrrrr[b] === 'X') {
+            b--;
+        }
+        id = arrrrrrr[b];
+        arrrrrrr[i] = arrrrrrr[b];
+        arrrrrrr[b] = 'X';
+    }
+    sum += id * i;
+}
+
+console.log('sum ' + sum);
